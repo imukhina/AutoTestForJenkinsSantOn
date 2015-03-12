4385801eac9169a78b8ef083efd2aaab9f110681
adventusful <lemonad_11_90@mail.ru>
2015-03-12 14:56:13 +0200

    Update AutoTestFullPurchasePack.php

--- AutoTestFullPurchasePack.php
+++ AutoTestFullPurchasePack.php
@@ -3,58 +3,57 @@ require_once 'AutoTestFullPurchase.php';
 
 class AutoTestFullPurchasePack extends AutoTestFullPurchase
 {
-/**
- * @expectedException Exception
- */
+
 	
     public function testFullPurchasePack() 
     {	
 		$this->regFullPurch();
 		$ftm = array(
-		/* "//a[@title='Мебель для ванной']"
+		 "//a[@title='Мебель для ванной']" , 
 		   "//a[@title='Раковины']",
            "//a[@title='Унитазы']",
 		   "//a[@title='Писсуары']",
 		   "//a[@title='Кухонные мойки']",
 		   "//a[@title='Смесители']",
 		   "//a[@title='Душевая программа']",
-		   "//a[@title='Биде']",
 		   "//a[@title='Инсталляции']",
 		   "//a[@title='Сифоны']",
 		   "//a[@title='Полотенцесушители']",
 		   "//a[@title='Водонагреватели']",
-		   "//a[@title='Радиаторы отопления']",
 		   "//a[@title='Обогреватели']",
-		   "//a[@title='Теплые полы']", */
-		   "//a[@title='Трапы и душевые лотки']"
+		   "//a[@title='Радиаторы отопления']",
+		   "//a[@title='Теплые полы']" ,
+		   "//a[@title='Трапы и душевые лотки']" , 
+		   "//a[@title='Биде']"
+		   
 		   );
-        $this->FullPurch($ftm);
+       $this->FullPurchase($ftm);
 		sleep(5);
-		$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Душевые кабины и углы']"));
+	 	$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Душевые кабины и углы']"));
 		$DiscSale->click();
 		sleep(5);
 		$sb = array(
-	        /*   "//a[@title='Душевые кабины']",
-			   "//a[@title='Душевые боксы']", */
+	           "//a[@title='Душевые кабины']",
+			   "//a[@title='Душевые боксы']", 
 			   "//a[@title='Уголки, ограждения, поддоны']"
 		);
-		 $this->FullPurch($sb);
+		 $this->FullPurchase($sb);
 		sleep(5);
-	/*	$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Ванны']"));
+		$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Ванны']"));
 		$DiscSale->click();
 		sleep(5);
 		$bath = array(
 	           "//a[@title='Акриловые ванны']",
 			   "//a[@title='Чугунные ванны']",
 			   "//a[@title='Стальные ванны']", 
-			   "//a[@title='Ванны из литьевого мрамора']"
+			   "//a[@title='Ванны из литьевого мрамора']" 
 		);
-		$this->FullPurch($bath);
+		$this->FullPurchase($bath);
 		sleep(3);
-		$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Комплектующие']"));
+	 	$DiscSale = $this->webDriver->findElement(WebDriverBy::xpath("//a[@title='Комплектующие']"));
 		$DiscSale->click();
 		$equip = array(
-	        /*   "//a[@title='для мебели']",
+	          "//a[@title='для мебели']",
 			   "//a[@title='для душевых кабин']",
 			   "//a[@title='для ванн']",
 			   "//a[@title='для унитазов']",
@@ -65,8 +64,8 @@ class AutoTestFullPurchasePack extends AutoTestFullPurchase
 			   "//a[@title='для писсуаров']", 
 			   "//a[@title='для теплотехники']"
 		);
-		$this->FullPurch($equip);
-		sleep(3); */
+		$this->FullPurchase($equip);
+		sleep(3); 
     }
 	
 }
